96b62612e4746a15c95e5e0b85dacde75297af1e
cpindayddam1 <cpindayddam1@gmail.com>
2023-03-14 23:15:58 +0100

    obtener cod municipio

--- proyecto/lib/screens/3_home_screen.dart
+++ proyecto/lib/screens/3_home_screen.dart
@@ -6,6 +6,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
 import 'package:proyecto/models/aemet_rest.dart';
+import 'package:proyecto/models/codigos_municipios.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,11 +23,9 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   // HomeScreenState();
 
-  String nvlRiesgo = 'Nivel';
-  Color colorRiesgo = Colors.white;
-  String recomendaciones = '';
+
   Container containerResultado = Container(
-    child: const Text('Da clic en el boton para obtener datos'),
+    child: Text('Da clic en el boton para obtener datos'),
   );
 
   //map con los niveles de actividad y limites wbgt
@@ -46,17 +45,20 @@ class HomeScreenState extends State<HomeScreen> {
     'extremo': ['PARAR la actividad al sentir calambres, mareos, fatiga, nauseas.', 'Retirarse a un lugar fresco y comunicar la situación a un compañero o superior.', 'Si los síntomas persisten avisar al 112.']
   };
 
+  String x = CodigosMunicipios.codigos['municipios'][10]['CMUN'];
+
   @override
   Widget build(BuildContext context) {
     // String str = "";
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 100, 
         title: const Text('AEMET Datos'),
         backgroundColor: Color.fromARGB(255, 94, 24, 110),
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+          // padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
           child: Column(
             children: [
               Container(
@@ -68,7 +70,7 @@ class HomeScreenState extends State<HomeScreen> {
               const SizedBox(height: 30),
               ElevatedButton(
                 onPressed: () {
-                  obtenerRecomendaciones(1, '', true);
+                  obtenerWidgetResultado();
                 },
                 child: const SizedBox(
                   height: 50,
@@ -78,83 +80,183 @@ class HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               const SizedBox(height: 30),
-              Container(
-                alignment: Alignment.center,
-                height: 30,
-                child: const Text('Nivel de riesgo', style: TextStyle(fontSize: 20, color: Colors.amber)),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, 'singup');
+                },
+                child: const SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: Center(
+                    child: Text('Volver al formulario', textAlign: TextAlign.center)),
+                ),
               ),
               const SizedBox(height: 30),
-              Container(
-                alignment: Alignment.center,
-                height: 30,
-                width: 300,
-                color: colorRiesgo,
-                child: Text(nvlRiesgo, style: TextStyle(fontSize: 20, color: Colors.white)),
-              ),
-              const SizedBox(height: 30),
-              Container(
-                alignment: Alignment.center,
-                height: 30,
-                child: const Text('Recomendaciones', style: TextStyle(fontSize: 20, color: Colors.amber)),
-              ),
-              const SizedBox(height: 30),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                alignment: Alignment.topCenter,
-                height: 400,
-                color: Colors.green,
-                child: Text(recomendaciones, style: TextStyle(fontSize: 15, color: Colors.white)),
-              ),
-        
-            ],
+              containerResultado,
+              Image(image: AssetImage('assets/ispln.png')),
+              Text(x)
+              ],
           ),
         ),
       )
     );
   }
 
-Future<void> x() async {
+  //pinta en pantalla el widget con el resultado o un mensaje de error
+  Future<void> obtenerWidgetResultado() async {
+    int hora = 12;
+    String nvlAct = 'pesado';
+    bool esVulnerable = true;
 
-}
-
-
-  // Future<AemetRest> fetchAemetRest(int? codMun) async {
-  //   String apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjcGluZGF5ZGVsQGVkdWNhY2lvbi5uYXZhcnJhLmVzIiwianRpIjoiZDMzNzQ4YWYtMzBiMC00ZWU5LWFmOGQtMjgzNDM2YjhkMWIxIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2Nzg0MDg0MTAsInVzZXJJZCI6ImQzMzc0OGFmLTMwYjAtNGVlOS1hZjhkLTI4MzQzNmI4ZDFiMSIsInJvbGUiOiIifQ.hZu4JaDTzSz854rZZR2D2pjZNgqJy0EpPkehi0cJQ8M';
-  //   int codigoMunicipio = 27025;
-
-  //   String urlStr = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/' + codigoMunicipio.toString();
-  //   urlStr += '/?api_key=' + apiKey;
-  //   //https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/27025/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjcGluZGF5ZGVsQGVkdWNhY2lvbi5uYXZhcnJhLmVzIiwianRpIjoiZDMzNzQ4YWYtMzBiMC00ZWU5LWFmOGQtMjgzNDM2YjhkMWIxIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2Nzg0MDg0MTAsInVzZXJJZCI6ImQzMzc0OGFmLTMwYjAtNGVlOS1hZjhkLTI4MzQzNmI4ZDFiMSIsInJvbGUiOiIifQ.hZu4JaDTzSz854rZZR2D2pjZNgqJy0EpPkehi0cJQ8M
-  //   final response = await http.get(Uri.parse(urlStr));
-
-  //   if (response.statusCode == 200) {
-  //     // Si la llamada al servidor fue exitosa, analiza el JSON
-  //     return AemetRest.fromJson(json.decode(response.body));
-  //   } else {
-  //     // Si la llamada no fue exitosa, lanza un error.
-  //     throw Exception('Failed to load post');
-  //   }
-  // }
-
-//indicar mensajes indicativos
-
-  Future<String> obtenerNombreMunicipio() async {
     String msjGPS = await obtenerUbicacionGPS();
     
-    if (msjGPS == 'El GPS está desactivado' || msjGPS == 'No se ha dado permiso para utilizar el GPS' ) {
-      return 'error gps';
-    }
-    
-    
+    // if (msjGPS == 'El GPS está desactivado' || msjGPS == 'No se ha dado permiso para utilizar el GPS' ) {
+    //   return 'error gps';
+    // }
     double lat = double.parse(msjGPS.split('*')[0]);
     double lon = double.parse(msjGPS.split('*')[1]);
 
-    //llamar api que devuelva nombre de municipio a partir de coordenadas
-    String url = '';
-    final response  = await http.get(Uri.parse(url));
+    int codMun = obtenerCodigoMunicipio(lat, lon);
+    // 27025
+    final res = await obtenerDatosAemet(hora, nvlAct, esVulnerable, codMun);
 
+    if (res.runtimeType.toString() == 'IdentityMap<String, dynamic>') {
+      String riesgo = res['nvlRiesgo'];
+          Color color = res['colorRiesgo'];
+          String temperatura = res['temperatura'];
+          List<String> recs = res['recomendaciones'];/////
+          String msjVul = res['mensajeVulnerable'];
 
-    return lat.toString() + ' - '  + lon.toString();
+          String str = '';
+          recs.forEach((r) => str += r + '\n\n\n');
+
+          Container container = 
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Container(
+              color: Color.fromARGB(255, 188, 219, 245),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  const SizedBox(height: 30),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    // height: 60,
+                    alignment: Alignment.centerLeft,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Container(child: Text('Datos generales', style: TextStyle(fontSize: 18, color: Colors.purple, fontWeight: FontWeight.bold))),
+                        const SizedBox(height: 30),
+                        Row(
+                          children: [
+                            Expanded(flex: 1, child: const Text('Nivel de riesgo', style: TextStyle(fontSize: 15, color: Colors.blue, fontWeight: FontWeight.bold), textAlign: TextAlign.start, )),
+                            Expanded(
+                              flex: 2,
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 30,
+                                color: color,
+                                child: Text(riesgo, style: TextStyle(fontSize: 20, color: Colors.white)),
+                              ),
+                            )]
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Expanded(flex: 1, child: const Text('Temperatura', style: TextStyle(fontSize: 15, color: Colors.blue, fontWeight: FontWeight.bold), textAlign: TextAlign.start,)),
+                            Expanded(
+                              flex: 2,
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 30,
+                                child: Text(temperatura, style: TextStyle(fontSize: 15, color: Colors.blue)),
+                              ),
+                            )]
+                        ),
+                      ]
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    // height: 60,
+                    alignment: Alignment.center,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Container(child: Text('Recomendaciones', style: TextStyle(fontSize: 18, color: Colors.purple, fontWeight: FontWeight.bold))),
+                        const SizedBox(height: 30),
+                        Container(alignment: Alignment.centerLeft, child: Text(str, style: TextStyle(fontSize: 15, color: Colors.blue))),
+                      ]
+                    ),
+                  ),
+                ]
+              ),
+            ),
+          );
+
+          containerResultado = container;
+          setState(() {});
+    }
+    else {
+      
+          Container container = 
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Container(
+              color: Colors.red,
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  const SizedBox(height: 30),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    // height: 60,
+                    alignment: Alignment.center,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Container(child: Text('Error', style: TextStyle(fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold))),
+                        const SizedBox(height: 30),
+                        Container(child: Text(res, style: TextStyle(fontSize: 15, color: Colors.amber))),
+                      ]
+                    ),
+                  )
+                ]
+              ),
+            ),
+          );
+
+      containerResultado = container;
+      setState(() {});
+    }
+  }
+  
+  int obtenerCodigoMunicipio(double lat, double lon) {
+    final lista = CodigosMunicipios.codigos['municipios'];
+
+    // iterar para obtener distancia mas cercana
+    double distanciaMenor = double.maxFinite;
+    int codMenor = 0;
+    // String nombre = '';
+    for(var obj in lista) {
+      double lat2 = double.parse(obj['LATITUD']);
+      double lon2 = double.parse(obj['LONGITUD']);
+      int cod = int.parse(obj['CPRO'] + obj['CMUN']);
+
+      double distancia = sqrt(pow((lat2 - lat), 2)  + pow((lon2 - lon), 2));
+
+      if (distancia < distanciaMenor) {
+        distanciaMenor = distancia;
+        codMenor = cod;
+        // nombre = obj['NOMBRE'];
+      }
+      // print(lat2.toString() + ' * ' + lon2.toString());
+    }
+    // print(nombre);
+    // print(lat.toString() + ' *** ' + lon.toString());
+    return codMenor;
   }
 
   Future<String> obtenerUbicacionGPS() async {
@@ -189,15 +291,11 @@ Future<void> x() async {
     texto =
         '${locationData.latitude}*${locationData.longitude}';
 
-    // 4: Recibujamos la pantalla cuando termine
-    // setState(() {});
     return texto;
   }
 
-  Future<String> obtenerUrlDatosAemet(int codMun) async {
+  Future<String> obtenerUrlDatos(int codMun) async {
     String apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjcGluZGF5ZGVsQGVkdWNhY2lvbi5uYXZhcnJhLmVzIiwianRpIjoiZDMzNzQ4YWYtMzBiMC00ZWU5LWFmOGQtMjgzNDM2YjhkMWIxIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2Nzg0MDg0MTAsInVzZXJJZCI6ImQzMzc0OGFmLTMwYjAtNGVlOS1hZjhkLTI4MzQzNmI4ZDFiMSIsInJvbGUiOiIifQ.hZu4JaDTzSz854rZZR2D2pjZNgqJy0EpPkehi0cJQ8M';
-    codMun = 27025;//////////////////////////////////////////////
-
     String urlStr = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/' + codMun.toString();
     urlStr += '/?api_key=' + apiKey;
     //https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/27025/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjcGluZGF5ZGVsQGVkdWNhY2lvbi5uYXZhcnJhLmVzIiwianRpIjoiZDMzNzQ4YWYtMzBiMC00ZWU5LWFmOGQtMjgzNDM2YjhkMWIxIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2Nzg0MDg0MTAsInVzZXJJZCI6ImQzMzc0OGFmLTMwYjAtNGVlOS1hZjhkLTI4MzQzNmI4ZDFiMSIsInJvbGUiOiIifQ.hZu4JaDTzSz854rZZR2D2pjZNgqJy0EpPkehi0cJQ8M
@@ -215,18 +313,14 @@ Future<void> x() async {
     }
   }
 
-  Future<void> obtenerRecomendaciones(int hora, String nvlAct, bool esVulnerable) async {
-      hora = 12;
-      nvlAct = 'pesado';
-      esVulnerable = true;
-
-      String url = await obtenerUrlDatosAemet(0);
-      print(url);
+  //deuelve los resultados aemet de la hora indicada
+  Future<dynamic> obtenerDatosAemet(int hora, String nvlAct, bool esVulnerable, int codMun) async {
+    try {
+      String url = await obtenerUrlDatos(codMun);
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
-        String nombre = jsonData[0]['nombre'];
 
         //calculamos el wbgt -----------------------------------------------------------
 
@@ -279,28 +373,26 @@ Future<void> x() async {
           msjVulnerable = 'Eres parte de la población vulnerable, toma precausiones.';
         }
 
+        //retornar map
+        Map<String, dynamic> resultados = {
+          'nvlRiesgo': riesgo.toUpperCase(),
+          'colorRiesgo': color,
+          'temperatura': ta.toString() + 'ºC',
+          'recomendaciones': recs,
+          'mensajeVulnerable': msjVulnerable
+        };
 
-        String str = recs.toString();
-        // for (String r in recs!) { 
-        //   str = r + ' - ';
-        // }
-        //devolvemos una lista con color de riesgo wbgt, temperatura actual, recomendaciones
-        //alto - red - temp actual - recomendaciones - si eres poblacion vulnerable
-        nvlRiesgo = riesgo.toUpperCase();
-        colorRiesgo = color;
-        recomendaciones = str;
-
-        // print(hr.toString());
-        setState(() {});
+        return resultados;
       }
       else {
       // Si la llamada no fue exitosa, lanza un error.
-      throw Exception('Failed to load post');
+      // throw Exception('Failed to load post');
+        return 'Error en la obtencion de datos';
       }
-
-
+    }
+    catch (e) {
+      return 'Algo salió mal. Inténtalo más tarde';
+    }     
+      
   }
-
 }
-
-//https://github.com/inigoflores/ds-codigos-postales-ine-es/blob/master/data/codigos_postales_municipios.csv
